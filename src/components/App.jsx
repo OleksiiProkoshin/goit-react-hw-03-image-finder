3d62f3a6bb93ce432f631e6581db8ae1d2b43041
OleksiiProkoshin <my.mail2210@gmail.com>
2023-09-04 22:22:41 +0300

    loading fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,7 @@ export class App extends Component {
     page: 1,
     apiKey: '38514161-f950138758225d897083d0d15',
     searchPerformed: false,
-      };
+  };
 
   componentDidMount() {
     axios.defaults.baseURL = 'https://pixabay.com/api/';
@@ -25,27 +25,39 @@ export class App extends Component {
   loadMore = () => {
     const { query, page, apiKey } = this.state;
     const nextPage = page + 1;
-  
+
     this.setState({ isLoading: true });
 
     axios
-      .get('', { params: { q: query, page: nextPage, key: apiKey, image_type: 'photo', orientation: 'horizontal', per_page: 12 } })
-      .then((response) => {
-        this.setState((prevState) => ({
+      .get('', {
+        params: {
+          q: query,
+          page: nextPage,
+          key: apiKey,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          per_page: 12,
+        },
+      })
+      .then(response => {
+        this.setState(prevState => ({
           images: [...prevState.images, ...response.data.hits],
           page: nextPage,
           isLoading: false,
         }));
       })
-      .catch((error) => {
+      .catch(error => {
         this.setState({ error, isLoading: false });
       });
   };
 
   handleSearchSubmit = query => {
-    this.setState({ query, images: [], isLoading: true, page: 1, searchPerformed: true }, () => {
-      this.loadMore();
-    });
+    this.setState(
+      { query, images: [], isLoading: true, page: 1, searchPerformed: true },
+      () => {
+        this.loadMore();
+      }
+    );
   };
 
   handleImageClick = selectedImage => {
@@ -62,16 +74,17 @@ export class App extends Component {
     return (
       <div className="App">
         <Searchbar onSubmit={this.handleSearchSubmit} isLoading={isLoading} />
-        {isLoading ? (
-          <LoadingIndicator />
-        ) : (
-          <>
-            <ImageGallery images={images} onImageClick={this.handleImageClick} searchPerformed={searchPerformed}/>
-            {images.length > 0 && (
-              <Button className="Button"onClick={this.loadMore} />
-            )}
-          </>
-        )}
+        {isLoading && <LoadingIndicator />}
+        <>
+          <ImageGallery
+            images={images}
+            onImageClick={this.handleImageClick}
+            searchPerformed={searchPerformed}
+          />
+          {images.length > 0 && (
+            <Button className="Button" onClick={this.loadMore} />
+          )}
+        </>
         {selectedImage && (
           <Modal image={selectedImage} onClose={this.closeModal} />
         )}
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -11,6 +11,7 @@ export const LoadingIndicator = () => {
         wrapperStyle={{}}
         wrapperClass=""
         visible={true}
+        
       />
     </div>
   );
